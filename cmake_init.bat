3d1a9edff4d82579281675d0669925157c8b7d42
chengang <chengang01@live.com>
2024-01-18 09:20:22 +0800

    Create cmake_init.bat

---
+++ cmake_init.bat
@@ -0,0 +1,4 @@
+rem cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -G "Ninja" -DCMAKE_INSTALL_PREFIX:PATH=instdir
+rem cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -G "Visual Studio 17 2022" -DCMAKE_INSTALL_PREFIX:PATH=instdir
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX:PATH=instdir
+pause
