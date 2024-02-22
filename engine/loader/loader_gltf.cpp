ee95ef30748acd12a872bad5e4a15385ce7e139a
chengang <chengang01@live.com>
2024-02-22 16:36:52 +0800

    can run

---
+++ engine/loader/loader_gltf.cpp
@@ -0,0 +1,33 @@
+#include"loader_gltf.h"
+#include "global.h"
+
+tinygltf::Model* loader_gltf::load(const std::string& filepath)
+{
+    if(filepath.empty())
+    {
+        return nullptr;
+    }
+    tinygltf::TinyGLTF loader;
+    std::string err;
+    std::string warn;
+    tinygltf::Model* pmodel=new tinygltf::Model;
+    bool ret = false;
+    if(global::core.endsWith(filepath, ".glb"))
+    {
+        // Assume binary glTF.
+        ret = loader.LoadBinaryFromFile(pmodel, &err, &warn, filepath);
+    }
+    else if(global::core.endsWith(filepath, ".gltf"))
+    {
+        // Assume ascii glTF.
+        ret = loader.LoadASCIIFromFile(pmodel, &err, &warn, filepath);
+    }
+    else
+    {
+        // Unknown file type.
+        delete pmodel;
+        pmodel=nullptr;
+        return nullptr;
+    }
+    return pmodel;
+}
---
+++ engine/render/render_object.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "shader.h"
+
+class render_object
+{
+    protected:
+        shader*  _pshader;
+        unsigned int _vbo;
+        unsigned int _vao;
+        unsigned int _ebo;
+        float* _vertices;
+        unsigned int* _indices;
+    public:
+        render_object() = default;
+        virtual ~render_object() = default;
+    public:
+        bool init(std::string& shader_path,float* vertices, unsigned int* indices);
+        void renderVBO();
+        void renderVAO();
+        void renderVEO();
+};
